ec01fec91f66c93cccc783dba7f626d951192952
Matt Lung <mlung1@gmail.com>
2013-03-27 23:50:02 -0400

    maps

--- resources/js/global.js
+++ resources/js/global.js
@@ -1,9 +1,22 @@
-﻿$(document).ready(function () {
+﻿// Wait for Cordova to load
+//
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// Cordova is ready
+//
+function onDeviceReady() {
     $(".ashmap").click(function () {
         event.preventDefault();
         window.open('http://maps.google.com/maps?f=d&amp;saddr=&amp;daddr=7609+West+Jefferson+Blvd+Fort+Wayne%2C+IN+46804&amp;hl=en&amp;geocode=&amp;mra=ls&amp;sll=39.916171,-86.098821&amp;sspn=0.011027,0.019226&amp;ie=UTF8&amp;z=16&amp;layer=c&amp;cbll=39.916176,-86.0988', '_system', 'location=yes');
     });
-});
+}
+
+//$(document).ready(function () {
+//    $(".ashmap").click(function () {
+//        event.preventDefault();
+//        window.open('http://maps.google.com/maps?f=d&amp;saddr=&amp;daddr=7609+West+Jefferson+Blvd+Fort+Wayne%2C+IN+46804&amp;hl=en&amp;geocode=&amp;mra=ls&amp;sll=39.916171,-86.098821&amp;sspn=0.011027,0.019226&amp;ie=UTF8&amp;z=16&amp;layer=c&amp;cbll=39.916176,-86.0988', '_system', 'location=yes');
+//    });
+//});
 
 //----------------------------------
 // Load HTML via jQuery AJAX Request
