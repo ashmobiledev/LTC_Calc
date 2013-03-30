165127da6d839898c7642621ecd6e0c4362525ed
Matt Lung <mlung1@gmail.com>
2013-03-30 19:33:01 -0400

    asdf

--- global.js
+++ global.js
@@ -12,7 +12,7 @@ function onDeviceReady() {
 $(document).ready(function () {
     $(".ashmap").click(function (e) {
         e.preventDefault();
-        openMap();
+        openInNativeBrowser('http://maps.google.com/maps?f=d&saddr=&daddr=7609+West+Jefferson+Blvd+Fort+Wayne%2C+IN+46804&hl=en&geocode=&mra=ls&sll=39.916171,-86.098821&sspn=0.011027,0.019226&ie=UTF8&z=16&layer=c&cbll=39.916176,-86.0988');
     });
 }); 
 
@@ -28,7 +28,7 @@ function checkConnection() {
     states[Connection.CELL_4G] = 'Cell 4G connection';
     states[Connection.NONE] = 'No network connection';
 
-    alert('Connection type: ' + states[networkState]);
+    //alert('Connection type: ' + states[networkState]);
 }
 
 
@@ -49,8 +49,8 @@ function loadURL(url) {
     return retVal;
 };
 
-function openMap() {
-    alert('hi');
-    var ref = window.open('http://maps.google.com/maps?f=d&amp;saddr=&amp;daddr=7609+West+Jefferson+Blvd+Fort+Wayne%2C+IN+46804&amp;hl=en&amp;geocode=&amp;mra=ls&amp;sll=39.916171,-86.098821&amp;sspn=0.011027,0.019226&amp;ie=UTF8&amp;z=16&amp;layer=c&amp;cbll=39.916176,-86.0988', '_system', 'location=yes');
+function openInNativeBrowser(link) {
+
+    var ref = window.open(link, '_system', 'location=yes');
     return false;
 }
